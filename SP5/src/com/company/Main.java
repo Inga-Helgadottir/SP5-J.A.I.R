c7f7afcbb49a11aa2c25e8f517430383007b5c31
Angeliza-Medina <78863493+Angeliza-Medina@users.noreply.github.com>
2021-05-12 19:41:32 +0200

    Connect sign in function to db

--- SP5/src/com/company/Main.java
+++ SP5/src/com/company/Main.java
@@ -9,6 +9,7 @@ public class Main {
     public static void main(String[] args) {
         SignIn signIn = new SignIn();
         signIn.sign();
+<<<<<<< Updated upstream
 //        DBConnector.testConnection();
 =======
 >>>>>>> Stashed changes
