a0f264b76827c25bf17c6dd78a98e5e5ba828438
Rabee604 <abla.rabee@hotmail.com>
2021-05-18 09:31:30 +0200

    Opgaven

--- SP5/src/com/gui/UserCard.java
+++ SP5/src/com/gui/UserCard.java
@@ -6,8 +6,32 @@ import java.awt.*;
 public class UserCard {
 
    public static JPanel createUserCard(){
-      JPanel infoCard = new JPanel();
-      infoCard.setBackground(ColorManager.cyan);
-      return infoCard;
+      JPanel userCard = new JPanel();
+      userCard.setLayout(null);
+      JLabel jLabelName = new JLabel("Name");
+      jLabelName.setFont(new Font("Arial", Font.PLAIN, 30));
+      jLabelName.setForeground(Color.black);
+      jLabelName.setSize(300, 50);
+      jLabelName.setLocation(100, 30);
+
+      JLabel jLabelMail = new JLabel("Mail");
+      jLabelMail.setFont(new Font("Arial", Font.PLAIN, 30));
+      jLabelMail.setForeground(Color.black);
+      jLabelMail.setSize(300, 50);
+      jLabelMail.setLocation(100, 100);
+      JButton signOut= new JButton("Sign out");
+      signOut.setForeground(Color.black);
+      signOut.setSize(100, 30);
+      signOut.setLocation(100, 170);
+      //JLabel jLabelGetName = new JLabel(Main.currentUser.getName());
+      //JLabel jLabelGetMail = new JLabel(Main.currentUser.getEmail());
+      userCard.setBackground(ColorManager.cyan);
+      userCard.add(jLabelName);
+      userCard.add(jLabelMail);
+      userCard.add(signOut);
+      signOut.addActionListener(e -> {
+         GUI.outSideAppContentLayout.show(GUI.outsideAppCard, "SIGN_IN_CARD");
+      });
+      return userCard;
    }
 }
